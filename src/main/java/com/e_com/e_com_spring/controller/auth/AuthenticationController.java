55437f00c40910df60808661c4fbc763132f83dc
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-10 21:48:55 +0000

    feat(auth): integrate authentication service into AuthenticationController for user registration

--- src/main/java/com/e_com/e_com_spring/controller/auth/AuthenticationController.java
+++ src/main/java/com/e_com/e_com_spring/controller/auth/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.e_com.e_com_spring.dto.auth.LoginPostDto;
 import com.e_com.e_com_spring.dto.auth.LoginResponseDto;
 import com.e_com.e_com_spring.dto.auth.RegisterPostDto;
 import com.e_com.e_com_spring.dto.auth.RegisterResponseDto;
+import com.e_com.e_com_spring.service.auth.IAuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthenticationController {
+    private final IAuthenticationService authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDto> register(RegisterPostDto postDto) {
-        return null;
+        return ResponseEntity.ok(authenticationService.register(postDto));
     }
 
     @PostMapping("/login")

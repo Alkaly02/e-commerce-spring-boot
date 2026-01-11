e630ab075ea3135231f7174425ba9b77dc1f484d
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-11 22:23:09 +0000

    feat(seeding): implement database seeding for roles and privileges

---
+++ src/main/java/com/e_com/e_com_spring/repository/seeding/RolesSeeder.java
@@ -0,0 +1,11 @@
+package com.e_com.e_com_spring.repository.seeding;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class PrivilegesSeeding implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}

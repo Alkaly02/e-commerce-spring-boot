6b2970f8b0eb58ee9eee5f61933a3aa13eecb3ba
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-14 16:38:27 +0000

    feat(test): integrate Testcontainers for PostgreSQL in AuthenticationControllerTest and update test configuration

--- src/test/java/com/e_com/e_com_spring/controller/auth/AuthenticationControllerTest.java
+++ src/test/java/com/e_com/e_com_spring/controller/auth/AuthenticationControllerTest.java
@@ -4,18 +4,19 @@ import com.e_com.e_com_spring.EComSpringApplication;
 import com.e_com.e_com_spring.dto.auth.RegisterPostDto;
 import com.e_com.e_com_spring.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -25,13 +26,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = EComSpringApplication.class
 )
 @AutoConfigureMockMvc
-@TestPropertySource(
-        locations = "classpath:application-integrationtest.yml"
-)
+//@TestPropertySource(
+//        locations = "classpath:application-integrationtest.yml"
+//)
 class AuthenticationControllerTest {
 
     @Autowired
@@ -43,10 +44,34 @@ class AuthenticationControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+
     RegisterPostDto registerPostDto;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @BeforeEach
     void setUp(){
+        RestAssured.baseURI = "http://localhost:" + port;
         registerPostDto = new RegisterPostDto();
         registerPostDto.setFirstName("Mocked firstName");
         registerPostDto.setLastName("Mocked lastName");

95929280f94ce1d0f6f9e4c22488f6e8a822c338
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-20 11:52:27 +0000

    refactor(service): simplify user enable logic in UserService by delegating to StatusHandler for improved readability and maintainability

--- src/main/java/com/e_com/e_com_spring/service/admin/user/handleStatus/IStatusHandler.java
+++ src/main/java/com/e_com/e_com_spring/service/admin/user/handleStatus/IStatusHandler.java
@@ -1,4 +1,7 @@
 package com.e_com.e_com_spring.service.admin.user.handleStatus;
 
+import com.e_com.e_com_spring.model.User;
+
 public interface IStatusHandler {
+    User handleStatus(Long userId, boolean enable);
 }
--- src/main/java/com/e_com/e_com_spring/service/admin/user/handleStatus/StatusHandler.java
+++ src/main/java/com/e_com/e_com_spring/service/admin/user/handleStatus/StatusHandler.java
@@ -1,4 +1,27 @@
 package com.e_com.e_com_spring.service.admin.user.handleStatus;
 
-public class StatusHandler {
+import com.e_com.e_com_spring.exception.CustomException;
+import com.e_com.e_com_spring.model.User;
+import com.e_com.e_com_spring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class StatusHandler implements IStatusHandler{
+    private final UserRepository userRepository;
+
+    @Override
+    public User handleStatus(Long userId, boolean enable) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty()){
+            throw new CustomException("User does not exist", HttpStatus.NOT_FOUND);
+        }
+        var user = optionalUser.get();
+        user.setEnabled(enable);
+        return userRepository.save(user);
+    }
 }

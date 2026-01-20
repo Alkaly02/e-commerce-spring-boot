53c3535a7ff967a9a2b32d27c354f95b6e33d1ac
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-20 12:19:16 +0000

    refactor(service): remove unused imports and clean up UserService for improved code clarity

--- src/main/java/com/e_com/e_com_spring/service/admin/user/UserService.java
+++ src/main/java/com/e_com/e_com_spring/service/admin/user/UserService.java
@@ -1,19 +1,12 @@
 package com.e_com.e_com_spring.service.admin.user;
 
 import com.e_com.e_com_spring.dto.user.UserMiniDto;
-import com.e_com.e_com_spring.exception.CustomException;
 import com.e_com.e_com_spring.mapper.user.UserMapper;
 import com.e_com.e_com_spring.model.User;
-import com.e_com.e_com_spring.repository.UserRepository;
 import com.e_com.e_com_spring.service.admin.user.checker.IChecker;
 import com.e_com.e_com_spring.service.admin.user.handleStatus.IStatusHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor

f16e06d051ed5ba1a4735d99d6e718becbfee333
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-21 11:52:02 +0000

    feat(security): implement CustomUserDetails, JwtAuthenticationEntryPoint, and CustomUserDetailsService for enhanced user authentication and JWT handling

---
+++ src/main/java/com/e_com/e_com_spring/security/CustomUserDetails.java
@@ -0,0 +1,4 @@
+package com.e_com.e_com_spring.security;
+
+public class CustomUserDetails {
+}
--- src/main/java/com/e_com/e_com_spring/service/jwt/CustomUserDetailsService.java
+++ src/main/java/com/e_com/e_com_spring/service/jwt/CustomUserDetailsService.java
@@ -1,12 +1,21 @@
 package com.e_com.e_com_spring.service.jwt;
 
+import com.e_com.e_com_spring.model.User;
+import com.e_com.e_com_spring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CustomUserServiceDetails implements UserDetailsService {
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
         return null;
     }
 }

4a1045d57f8230bd0e5e99da8148d1547435deaa
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-22 10:53:17 +0000

    feat(service): add role validation in CreateUserService to ensure defined roles during user creation

--- src/main/java/com/e_com/e_com_spring/service/auth/createUser/CreateUserService.java
+++ src/main/java/com/e_com/e_com_spring/service/auth/createUser/CreateUserService.java
@@ -1,12 +1,14 @@
 package com.e_com.e_com_spring.service.auth.createUser;
 
 import com.e_com.e_com_spring.dto.auth.RegisterPostDto;
+import com.e_com.e_com_spring.exception.CustomException;
 import com.e_com.e_com_spring.mapper.user.UserMapper;
 import com.e_com.e_com_spring.model.Role;
 import com.e_com.e_com_spring.model.User;
 import com.e_com.e_com_spring.repository.RoleRepository;
 import com.e_com.e_com_spring.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,9 @@ public class CreateUserService implements ICreateUserService {
     public User create(RegisterPostDto postDto) {
         User newUser = userMapper.registerDtoToUser(postDto);
         Optional<Role> role = roleRepository.findByRoleType(newUser.getRole().getRoleType());
+        if (role.isEmpty()){
+            throw new CustomException(postDto.getRoleType() + " not defined", HttpStatus.BAD_REQUEST);
+        }
         newUser.setRole(role.get());
         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         newUser.setEnabled(true);

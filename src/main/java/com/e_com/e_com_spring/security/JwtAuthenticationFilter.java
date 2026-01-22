bf6ab9f79e45f8db56d4dbdb38507b59947a6324
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-01-22 12:17:47 +0000

    refactor(security): remove unused imports from JwtAuthenticationFilter to enhance code clarity

--- src/main/java/com/e_com/e_com_spring/security/JwtAuthenticationFilter.java
+++ src/main/java/com/e_com/e_com_spring/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.e_com.e_com_spring.security;
 
-import com.e_com.e_com_spring.exception.CustomException;
 import com.e_com.e_com_spring.service.jwt.IJwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -8,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;

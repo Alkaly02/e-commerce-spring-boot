310bbc5ca6288eef1ece3ae2404234655d89eb9c
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-02-04 10:59:22 +0000

    feat(category): add CategoryLogic service and ICategoryLogic interface for category update and deletion operations

---
+++ src/main/java/com/e_com/e_com_spring/service/admin/category/logic/CategoryLogic.java
@@ -0,0 +1,16 @@
+package com.e_com.e_com_spring.service.admin.category.logic;
+
+import com.e_com.e_com_spring.dto.category.CategoryGetDto;
+import com.e_com.e_com_spring.dto.category.CategoryPostDto;
+
+public class CategoryLogic implements ICategoryLogic {
+    @Override
+    public CategoryGetDto update(Long id, CategoryPostDto putDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}

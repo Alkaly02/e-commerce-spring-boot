2855467218510755840d159bec5028ca85d763bd
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-02-09 12:21:58 +0000

    test(category): extend CategoryControllerTest to utilize TestContainerConfig for PostgreSQL integration

--- src/test/java/com/e_com/e_com_spring/controller/TestContainerConfig.java
+++ src/test/java/com/e_com/e_com_spring/controller/TestContainerConfig.java
@@ -1,4 +1,25 @@
 package com.e_com.e_com_spring.controller;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@SpringBootTest
 public class TestContainerConfig {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @BeforeAll
+    static void beforeAll(){
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll(){
+        postgres.stop();
+    }
 }
--- src/test/java/com/e_com/e_com_spring/controller/admin/CategoryControllerTest.java
+++ src/test/java/com/e_com/e_com_spring/controller/admin/CategoryControllerTest.java
@@ -1,7 +1,24 @@
 package com.e_com.e_com_spring.controller.admin;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.e_com.e_com_spring.controller.TestContainerConfig;
+import com.e_com.e_com_spring.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-class CategoryControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+class CategoryControllerTest extends TestContainerConfig {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    
 }

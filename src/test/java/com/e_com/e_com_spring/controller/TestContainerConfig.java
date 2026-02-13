ac5e912e98dbd1fb14219d5422a7757f25ab7909
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2026-02-13 10:43:25 +0000

    refactor(tests): remove unused imports in TestContainerConfig and AuthenticationControllerTest

--- src/test/java/com/e_com/e_com_spring/controller/TestContainerConfig.java
+++ src/test/java/com/e_com/e_com_spring/controller/TestContainerConfig.java
@@ -1,7 +1,5 @@
 package com.e_com.e_com_spring.controller;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
